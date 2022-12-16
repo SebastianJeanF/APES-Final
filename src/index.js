fb950dfcc0d9eeecab5e3f132329b7d4508dde33
Sebastian <sebeld9@gmail.com>
2022-12-16 02:08:45 -0500

    1

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// function changeVisibility(event) {
+// 	//   if (d)
+// 	// document.getElementById(id).style.property = new style
+// 	console.log(event.path);
+//   for(let i = 0; i < event.path.length; i++) {
+//     if (event.path[i].nodeName === "SECTION"){
+
+//     }
+// }
+
+// //document.querySelector('#section').addEventListener('click', changeVisibility);
+
+// const highlightedItems = document.querySelectorAll('section');
+// highlightedItems.forEach((userItem) => {
+// 	userItem.addEventListener('click', changeVisibility);
+// });
